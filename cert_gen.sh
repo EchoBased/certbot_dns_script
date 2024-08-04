dcef241b31c31a745578117c6a67da3d9589f82e
Erik <103145299+EchoBased@users.noreply.github.com>
2024-08-03 21:58:41 -0500

    Update cert_gen.sh
    Uploaded current version

--- cert_gen.sh
+++ cert_gen.sh
@@ -1,9 +1,17 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "CERTBOT MANUAL DNS RENEW"
-echo "By: Erik Wright"
+read  -p "Enter domain: " domain
 
-echo "Enter domain: "
-read domain
-echo "Domain: $domain"
+echo $domain
+
 certbot --manual --preferred-challenges dns certonly -d $domain
+
+echo "Moving certs to live dir"
+
+cp /etc/letsencrypt/live/wrightphotos.ddns.net/chain.pem /var/snap/nextcloud/current/
+
+cp /etc/letsencrypt/live/wrightphotos.ddns.net/privkey.pem /var/snap/nextcloud/current/
+
+cp /etc/letsencrypt/live/wrightphotos.ddns.net/cert.pem /var/snap/nextcloud/current/
+
+nextcloud.enable-https custom -s /var/snap/nextcloud/current/cert.pem /var/snap/nextcloud/current/privkey.pem /var/snap/nextcloud/current/chain.pem 
