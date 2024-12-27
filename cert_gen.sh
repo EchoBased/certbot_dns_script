301dcb44c305f9bd9b789c319e2cecea25fd9549
Erik <103145299+EchoBased@users.noreply.github.com>
2024-12-27 17:38:18 -0600

    Update cert_gen.sh

--- cert_gen.sh
+++ cert_gen.sh
@@ -8,10 +8,10 @@ certbot --manual --preferred-challenges dns certonly -d $domain
 
 echo "Moving certs to live dir"
 
-cp /etc/letsencrypt/live/wrightphotos.ddns.net/chain.pem /var/snap/nextcloud/current/
+cp /etc/letsencrypt/live/YOURSITE.net/chain.pem /var/snap/nextcloud/current/
 
-cp /etc/letsencrypt/live/wrightphotos.ddns.net/privkey.pem /var/snap/nextcloud/current/
+cp /etc/letsencrypt/live/YOURSITE.net/privkey.pem /var/snap/nextcloud/current/
 
-cp /etc/letsencrypt/live/wrightphotos.ddns.net/cert.pem /var/snap/nextcloud/current/
+cp /etc/letsencrypt/live/YOURSITE.net.net/cert.pem /var/snap/nextcloud/current/
 
 nextcloud.enable-https custom -s /var/snap/nextcloud/current/cert.pem /var/snap/nextcloud/current/privkey.pem /var/snap/nextcloud/current/chain.pem 
